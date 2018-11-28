ef11db675d1e1706e510e6c81dae530fdb70f6f8
Jack Foltz <jack@foltz.io>
2018-11-27 20:22:54 -0500

    Finish python thing

--- raytrace.py
+++ raytrace.py
@@ -9,11 +9,11 @@ import multiprocessing
 def normalize(v):
     return v / np.linalg.norm(v) 
 
-scene_width = 256
+scene_width = 1024
 scene_halfwidth = scene_width / 2
 scene_pixels = scene_width ** 2
 
-camera_pos = np.array([0, 0, 0])
+camera_pos = np.array([0, 1, 0])
 camera_lookat = np.array([0, 0, 4])
 camera_focal = 1
 
@@ -38,13 +38,40 @@ class Light:
     def __init__(self, dir, color, intensity):
         self.dir = dir
         self.color = color
-        self.intensity = intensity
+        self.inten = intensity
 
+    def distance(self, pos):
+        return math.inf
+
+    def direction(self, pos):
+        return -self.dir
+
+    def intensity(self, pos):
+        return self.inten
+
+class PointLight:
+    def __init__(self, pos, color, intensity):
+        self.pos = pos
+        self.color = color
+        self.inten = intensity
+
+    def distance(self, pos):
+        between = self.pos - pos
+        return np.linalg.norm(between)
+
+    def direction(self, pos):
+        return normalize(self.pos - pos)
+
+    def intensity(self, pos):
+        radius = np.linalg.norm(self.pos - pos)
+        return self.inten / (4 * math.pi * radius ** 2)
 
 class Material:
-    def __init__(self, color, albedo):
+    def __init__(self, color, albedo, reflectivity, index):
         self.color = color
         self.albedo = albedo
+        self.reflectivity = reflectivity
+        self.index = index
 
 
 class Ray:
@@ -70,6 +97,23 @@ class Sphere:
         between = ray.pos - self.pos
         return normalize(between)
 
+    def intersect(self, ray):
+        between = self.pos - ray.pos
+        adj = np.dot(between, ray.dir)
+        dist = math.sqrt(np.dot(between, between) - (adj ** 2))
+
+        if dist > self.radius:
+            return None
+
+        chord = math.sqrt(self.radius ** 2 - dist ** 2)
+        t0 = adj - chord
+        t1 = adj + chord
+
+        if t0 < 0 and t1 < 0:
+            return None
+
+        t = t0 if t0 < t1 else t1        
+        return ray.pos + (ray.dir * t)
 
 class Plane:
     def __init__(self, pos, norm, material):
@@ -89,55 +133,132 @@ class Plane:
     def normal(self, ray):
         return self.norm
 
-scene_global_light = Light(vec(0, -1, 0), vec(1, 1, 1), 50)
+    def intersect(self, ray):
+        return ray.pos + (ray.dir * self.distance(ray))
+
+scene_lights = [
+    Light(vec(0, -1, 0.4), vec(1, 1, 1), 8),
+    PointLight(vec(0, 0, 3), vec(0.8, 0.3, 0.3), 1000)
+]
         
 scene_spheres = [
-    Sphere(np.array([0, 0, 5]), 1, Material(np.array([0.15, 0.8, 0.15]), 0.18)),
-    Sphere(np.array([-3, 1, 6]), 2, Material(np.array([1.0, 0.2, 0.2]), 0.18)),
-    Sphere(np.array([2, 1, 4]), 1.5, Material(np.array([0.2, 0.2, 1.0]), 0.18))
+    Sphere(np.array([0, 0, 5]), 1, Material(np.array([0.15, 0.8, 0.15]), 0.18, 0.94, 0)),
+    Sphere(np.array([3, 1, 6]), 2, Material(np.array([1.0, 0.2, 0.2]), 0.18, 0, 0)),
+    Sphere(np.array([-2, 1, 4]), 1.5, Material(np.array([0.2, 0.2, 1.0]), 0.18, 0, 0))
 ]
 
 scene_planes = [
-    Plane(vec(0, -2, 0), vec(0, 1, 0), Material(vec(0.13, 0.13, 0.13), 0.18)),
-    Plane(vec(0, 0, 20), vec(0, 0, -1), Material(vec(0.5, 0.5, 0.5), 0.18))
+    Plane(vec(0, 0, 10), vec(0, 0, -1), Material(vec(0.6, 0.9, 0.9), 0.18, 0, 0)),
+    Plane(vec(0, -2, 0), vec(0, 1, 0), Material(vec(0.13, 0.13, 0.13), 0.18, 0, 0))
 ]
 
-def nearest_object(ray):
+def nearest_object(ray, exclude = None):
     scene_objects = scene_spheres + scene_planes
+    if exclude:
+        scene_objects = [obj for obj in scene_objects if obj is not exclude]
     object_distances = [(obj, obj.distance(ray)) for obj in scene_objects]
     return min(object_distances, key=lambda pair: pair[1])
 
-def trace(ray, steps = 0, total_distance = 0, tf = False):
-    obj, distance = nearest_object(ray)
-    if steps == 200 or ray.pos[2] > 40 or distance > 100:
-        if tf:
-            return False
-        return np.array([0, 0, 0])
-    elif distance < 0.001:
-        if tf:
-            return True
-        # cool stuff here
-        surface_normal = obj.normal(ray)
-        light_direction = -scene_global_light.dir
+def transmission_ray(normal, incident, point, index):
+    ref = normal
+    n_t = index 
+    n_i = 1
 
-        shadow_ray = Ray(ray.pos + (ray.dir * distance) + (surface_normal * 0.000001), light_direction)
-
-        light_intensity = scene_global_light.intensity
-        if trace(shadow_ray, 0, 0, True):
-            light_intensity = 0
-        else:
-            if isinstance(obj, Sphere):
-                print('nope')
+    dot = np.dot(incident, normal)
+    if dot < 0.0:
+        dot = -dot
+    else:
+        ref = -ref
+        n_t, n_i = n_i, n_t
             
-        light_power = max(np.dot(surface_normal, light_direction), 0.0) * light_intensity
-        light_reflected = obj.material.albedo / math.pi
-        light_color = scene_global_light.color
-        ambient = vec(0.2, 0.2, 0.2)
-        color = obj.material.color * light_color * light_power * light_reflected
+    ratio = n_i / n_t
+    k = 1 - (ratio ** 2) * (1 - np.dot(dot, dot) ** 2)
+    if k < 0:
+        print('Ah fuk')
+        return None
+    else:
+        return Ray(point, (point + dot * ref) * ratio - ref * math.sqrt(k))
+
+def fresnel(normal, incident, index):
+    dot = np.dot(incident, normal)
+    n_t = index
+    n_i = 1
+
+    if dot > 0:
+        n_t, n_i = n_i, n_t
+
+    sin_t = math.sqrt(max(n_i / n_t * (1 - dot ** 2), 0))
+    if sin_t > 1.0:
+        return 1
+    else:
+        cos_t = math.sqrt(max(1 - sin_t ** 2, 0))
+        cos_i = abs(cos_t)
+        r_s = ((n_t * cos_i) - (n_i * cos_t)) / ((n_t * cos_i) + (n_i * cos_t))
+        r_p = ((n_i * cos_i) - (n_t * cos_t)) / ((n_i * cos_i) + (n_t * cos_t))
+        return (r_s * r_s + r_p * r_p) / 2
+
+max_recursion_depth = 200
+
+def trace(ray, steps = 0, total_distance = 0, tf = False, exclude = None):
+    obj, distance = nearest_object(ray, exclude)
+    if steps == max_recursion_depth or ray.pos[2] > 100 or distance > 100:
+        if tf:
+            return (False, None)
+        return np.array([0, 0, 0])
+    elif distance < 0.0001:
+        if tf:
+            return (True, total_distance)
+        
+        surface_normal = obj.normal(ray)
+
+        ambient = 0.1
+        color = obj.material.color * ambient
+
+        # Lighting
+        for light in scene_lights:
+            light_direction = light.direction(ray.pos)
+
+            shadow_ray = Ray(ray.pos, light_direction)
+            shadow_trace = trace(shadow_ray, 0, 0, True, obj)
+            
+            in_light = not shadow_trace[0] or shadow_trace[1] > light.distance(ray.pos)
+            light_intensity = light.intensity(ray.pos) if in_light else 0
+            light_power = max(np.dot(surface_normal, light_direction), 0) * light_intensity
+            light_reflected = obj.material.albedo / math.pi
+
+            light_color = light.color * light_power * light_reflected
+
+            color = color + obj.material.color * light_color
+
+            
+        if obj.material.reflectivity is not 0:
+            reflectivity = obj.material.reflectivity
+            reflection_ray = Ray(ray.pos, ray.dir - (2 * np.dot(ray.dir, surface_normal) * surface_normal))
+            color = color * (1 - reflectivity)
+            color = color + trace(reflection_ray, steps + 1, 0, False, obj) * reflectivity;
+        elif obj.material.index is not 0:
+            surface_color = obj.material.color
+            refract_color = vec(0, 0, 0)
+
+            ray = transmission_ray(surface_normal, ray.dir, ray.pos, obj.material.index)
+            color = trace(ray, steps + 1, 0, False, obj) 
+            
+            #kr = fresnel(ray.dir, surface_normal, obj.material.index)
+            #if kr < 1:
+            #    ray = transmission_ray(surface_normal, ray.dir, ray.pos, obj.material.index)
+            #    refract_color = trace(ray, steps + 1, 0, False, obj)
+
+            #reflection_ray = Ray(ray.pos, ray.dir - (2 * np.dot(ray.dir, surface_normal) * surface_normal))
+            #reflection_color = trace(reflection_ray, steps + 1, 0, False, obj)
+
+            #color = reflection_color * kr + refract_color * (1 - kr)
+            #transparency = 1.0
+            #color = color * transparency * surface_color
+            
         return np.clip(color, 0, 1)
     else:
         ray.iterate(distance)
-        return trace(ray, steps + 1, total_distance + distance, tf)
+        return trace(ray, steps + 1, total_distance + distance, tf, exclude)
 
 def sample(x, y):
     return trace(Ray(camera_pos, ray_direction(x, y))) * 255
